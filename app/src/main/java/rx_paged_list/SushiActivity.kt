4c5b89602f18bc0429bb4d349a65f80f64878a36
Tan Jun Rong <tanjunrong@gmail.com>
2018-08-30 01:22:00 +0900

    better code

--- app/src/main/java/rx_paged_list/SushiActivity.kt
+++ app/src/main/java/rx_paged_list/SushiActivity.kt
@@ -6,49 +6,43 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.widget.Toast
 import beepbeep.pixelsforreddit.Sushi
+import com.jakewharton.rxbinding2.view.RxView
 import io.reactivex.Observable
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.Disposables
 import io.reactivex.functions.BiFunction
 import io.reactivex.schedulers.Schedulers
-import io.reactivex.subjects.PublishSubject
 import kotlinx.android.synthetic.main.activity_sushi.*
 import rx_paged_list.SushiDataSourceFactory
 
 class SushiActivity : AppCompatActivity() {
     var disposable = Disposables.empty()
-    val buttonSubject = PublishSubject.create<Unit>()
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sushi)
-        load()
+    private val buttonObservable by lazy {
+        RxView.clicks(sushiButton).map { Unit }.subscribeOn(AndroidSchedulers.mainThread())
     }
 
-    fun load() {
+
+    private val pagedListObservable by lazy {
         val config: PagedList.Config = PagedList.Config.Builder()
                 .setInitialLoadSizeHint(2)
                 .setPageSize(2)
                 .build()
+        RxPagedListBuilder(SushiDataSourceFactory(), config).buildObservable()
+    }
 
-        sushiButton.setOnClickListener {
-            buttonSubject.onNext(Unit)
-        }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sushi)
 
-        val obs = RxPagedListBuilder(SushiDataSourceFactory(), config)
-                .buildObservable()
-
-        disposable = Observable.combineLatest<PagedList<Sushi>, Unit, String>(obs, buttonSubject,
+        disposable = Observable.combineLatest<PagedList<Sushi>, Unit, String>(pagedListObservable, buttonObservable,
                 BiFunction { pagedList, _ ->
                     pagedList.loadAround(pagedList.size - 1)
                     pagedList.snapshot().fold("") { output, element -> "${output}, ${element.name}" }.drop(1)
                 })
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(
-                        {
-                            sushiTextView.text = it
-                        },
+                .subscribe({ sushiTextView.text = it },
                         {
                             it.printStackTrace()
                             Toast.makeText(this@SushiActivity, "BOOM! See log for error", Toast.LENGTH_SHORT).show()
